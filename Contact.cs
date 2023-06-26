2e4239ec5b679543392ee3f11713fc19c66d99f8
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-26 21:18:17 +0530

    added regualr expression to addressbook

--- Contact.cs
+++ Contact.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Reflection.Emit;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -20,6 +21,15 @@ namespace AddressBook
 
         public Contact(string name, string email, string phone, string state, string city, string zipcode)
         {
+            if (!IsValidEmail(email))
+                throw new ArgumentException("Invalid email address");
+
+            if (!IsValidPhone(phone))
+                throw new ArgumentException("Invalid phone number");
+
+            if (!IsValidZipcode(zipcode))
+                throw new ArgumentException("Invalid ZIP code");
+
             this.name = name;
             this.email = email;
             this.phone = phone;
@@ -27,10 +37,31 @@ namespace AddressBook
             this.city = city;
             this.zipcode = zipcode;
         }
+
         public override string ToString()
         {
             return $"Name: {name}\nEmail: {email}\nPhone: {phone}\nState: {state}\nCity: {city}\nZip: {zipcode}";
         }
 
+        private static bool IsValidEmail(string email)
+        {
+            // Regular expression to validate email addresses
+            string emailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+
+        private static bool IsValidPhone(string phone)
+        {
+            // Regular expression to validate phone numbers
+            string phonePattern = "^\\d{10}$";
+            return Regex.IsMatch(phone, phonePattern);
+        }
+
+        private static bool IsValidZipcode(string zipcode)
+        {
+            // Regular expression to validate ZIP codes (5 digits)
+            string zipcodePattern = @"^\d{5}$";
+            return Regex.IsMatch(zipcode, zipcodePattern);
+        }
     }
 }
